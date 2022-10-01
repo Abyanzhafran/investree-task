baa5d1d0eb346b446d929551ce98ab39ba688ede
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-10-01 13:49:24 +0700

    merge TransaksiService soal-empat

--- src/main/java/com/investree/demo/view/TransaksiService.java
+++ src/main/java/com/investree/demo/view/TransaksiService.java
@@ -1,14 +1,11 @@
 package com.investree.demo.view;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 import com.investree.demo.model.Transaksi;
 
 import java.util.Map;
 
 public interface TransaksiService {
-  public Map save(Transaksi transaksi);
+  public Map save(Transaksi objModel);
 
-  public Map updateStatus(Transaksi transaksi);
+  public Map updateStatus(Transaksi objModel);
 }
