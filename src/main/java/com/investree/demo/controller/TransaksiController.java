1174923d9a27e4f1155ba5f706320d365d6d825a
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-10-01 11:28:16 +0700

    get done C point form 4th question

--- src/main/java/com/investree/demo/controller/TransaksiController.java
+++ src/main/java/com/investree/demo/controller/TransaksiController.java
@@ -2,8 +2,13 @@ package com.investree.demo.controller;
 
 import com.investree.demo.view.TransaksiService;
 import com.investree.demo.model.Transaksi;
+import com.investree.demo.repository.TransaksiRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +21,9 @@ public class TransaksiController {
   @Autowired
   public TransaksiService transaksiService;
 
+  @Autowired
+  public TransaksiRepository transaksiRepository;
+
   @PostMapping("")
   public ResponseEntity<Map> save(@RequestBody Transaksi objModel) {
     Map save = transaksiService.save(objModel);
@@ -26,5 +34,20 @@ public class TransaksiController {
   public ResponseEntity<Map> updateStatus(@RequestBody Transaksi objModel) {
     Map update = transaksiService.updateStatus(objModel);
     return new ResponseEntity<Map>(update, HttpStatus.OK);
+  }
+
+  @GetMapping("/list")
+  public ResponseEntity<Map> list(
+      @RequestParam(required = false) Integer page,
+      @RequestParam(required = false) Integer size,
+      @RequestParam(required = false) String status) {
+    Pageable show_data = PageRequest.of(page, size);
+    Page<Transaksi> list = null;
+    if (page != null && size != null && status != null) {
+      list = transaksiRepository.findByStatusLike("%" + status + "%", show_data);
+    } else {
+      list = transaksiRepository.findAll(show_data);
+    }
+    return new ResponseEntity(list, new HttpHeaders(), HttpStatus.OK);
   }
 }
--- src/main/java/com/investree/demo/repository/TransaksiRepository.java
+++ src/main/java/com/investree/demo/repository/TransaksiRepository.java
@@ -2,14 +2,23 @@ package com.investree.demo.repository;
 
 import com.investree.demo.model.Transaksi;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 @Repository
-public interface TransaksiRepository extends JpaRepository<Transaksi, Long> {
+public interface TransaksiRepository extends PagingAndSortingRepository<Transaksi, Long> {
+
+  @Query("SELECT c FROM Transaksi c WHERE c.id = :id")
+  public Transaksi getById(@Param("id") long id);
+
   @Query("SELECT c FROM Transaksi c")
-  public List<Transaksi> getList();
+  Page<Transaksi> getList(Pageable pageable);
+
+  @Query("SELECT c FROM Transaksi c WHERE c.status LIKE :status")
+  Page<Transaksi> findByStatusLike(String status, Pageable pageable);
+
 }
